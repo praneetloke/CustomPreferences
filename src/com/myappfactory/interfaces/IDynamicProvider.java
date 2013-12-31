7dae359a7d60ecc0f7ed2be9be9a4c6f8986e277
Praneet Loke <praneetloke@hotmail.com>
2013-12-31 17:56:28 -0500

    Must have missed copying this change. Corrected the interface name.

--- src/com/myappfactory/interfaces/IDynamicProvider.java
+++ src/com/myappfactory/interfaces/IDynamicProvider.java
@@ -5,7 +5,7 @@ import java.util.List;
 /**
  * Created by admin on 12/29/13.
  */
-public interface DynamicEntriesProvider {
+public interface IDynamicProvider {
     public int getCount ();
     public List<String> getItems ();
     public void populate ();
